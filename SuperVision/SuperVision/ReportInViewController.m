f09e3fc84fff66649afa6ec92e2375c1b6cd6dd9
Ashish ojha <ojha.ashish13@gmail.com>
2015-04-07 23:51:24 +0530

    Image rotation issue fixed

--- SuperVision/SuperVision/ReportInViewController.m
+++ SuperVision/SuperVision/ReportInViewController.m
@@ -190,11 +190,12 @@ NSString * const DeviceMode = @"Device";
 //    NSString *filePath = [documentsPath stringByAppendingPathComponent:@"image.png"]; //Add the file name
 //    [pngData writeToFile:filePath atomically:YES]; //Write the file
 
-    
+    UIImage *rotatedImage = [self imageByRotatingImage:self.cameraImage fromImageOrientation:UIImageOrientationRight];
+
     
     
     SVNetworkApi *networkApi = [[SVNetworkApi alloc] init];
-    NSData *imageData = UIImagePNGRepresentation(self.cameraImage);
+    NSData *imageData = UIImagePNGRepresentation(rotatedImage);
     if(imageData){
         [networkApi uploadImage:imageData completionHandler:^(NSString *imageName, NSError *error) {
             NSLog(@"Image Upload Api Call");
@@ -208,6 +209,124 @@ NSString * const DeviceMode = @"Device";
     });
     
 
+}
+
+
+-(UIImage*)imageByRotatingImage:(UIImage*)initImage fromImageOrientation:(UIImageOrientation)orientation
+{
+    CGImageRef imgRef = initImage.CGImage;
+    
+    CGFloat width = CGImageGetWidth(imgRef);
+    CGFloat height = CGImageGetHeight(imgRef);
+    
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    CGRect bounds = CGRectMake(0, 0, width, height);
+    CGSize imageSize = CGSizeMake(CGImageGetWidth(imgRef), CGImageGetHeight(imgRef));
+    CGFloat boundHeight;
+    UIImageOrientation orient = orientation;
+    switch(orient) {
+            
+        case UIImageOrientationUp: //EXIF = 1
+            return initImage;
+            break;
+            
+        case UIImageOrientationUpMirrored: //EXIF = 2
+            transform = CGAffineTransformMakeTranslation(imageSize.width, 0.0);
+            transform = CGAffineTransformScale(transform, -1.0, 1.0);
+            break;
+            
+        case UIImageOrientationDown: //EXIF = 3
+            transform = CGAffineTransformMakeTranslation(imageSize.width, imageSize.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationDownMirrored: //EXIF = 4
+            transform = CGAffineTransformMakeTranslation(0.0, imageSize.height);
+            transform = CGAffineTransformScale(transform, 1.0, -1.0);
+            break;
+            
+        case UIImageOrientationLeftMirrored: //EXIF = 5
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeTranslation(imageSize.height, imageSize.width);
+            transform = CGAffineTransformScale(transform, -1.0, 1.0);
+            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
+            break;
+            
+        case UIImageOrientationLeft: //EXIF = 6
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeTranslation(0.0, imageSize.width);
+            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
+            break;
+            
+        case UIImageOrientationRightMirrored: //EXIF = 7
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeScale(-1.0, 1.0);
+            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
+            break;
+            
+        case UIImageOrientationRight: //EXIF = 8
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeTranslation(imageSize.height, 0.0);
+            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
+            break;
+            
+        default:
+            [NSException raise:NSInternalInconsistencyException format:@"Invalid image orientation"];
+            
+    }
+    // Create the bitmap context
+    CGContextRef    context = NULL;
+    void *          bitmapData;
+    int             bitmapByteCount;
+    int             bitmapBytesPerRow;
+    
+    // Declare the number of bytes per row. Each pixel in the bitmap in this
+    // example is represented by 4 bytes; 8 bits each of red, green, blue, and
+    // alpha.
+    bitmapBytesPerRow   = (bounds.size.width * 4);
+    bitmapByteCount     = (bitmapBytesPerRow * bounds.size.height);
+    bitmapData = malloc( bitmapByteCount );
+    if (bitmapData == NULL)
+    {
+        return nil;
+    }
+    
+    // Create the bitmap context. We want pre-multiplied ARGB, 8-bits
+    // per component. Regardless of what the source image format is
+    // (CMYK, Grayscale, and so on) it will be converted over to the format
+    // specified here by CGBitmapContextCreate.
+    CGColorSpaceRef colorspace = CGImageGetColorSpace(imgRef);
+    context = CGBitmapContextCreate (bitmapData,bounds.size.width,bounds.size.height,8,bitmapBytesPerRow,
+                                     colorspace, kCGBitmapAlphaInfoMask & kCGImageAlphaPremultipliedLast);
+    
+    if (context == NULL)
+        // error creating context
+        return nil;
+    
+    CGContextScaleCTM(context, -1.0, -1.0);
+    CGContextTranslateCTM(context, -bounds.size.width, -bounds.size.height);
+    
+    CGContextConcatCTM(context, transform);
+    
+    // Draw the image to the bitmap context. Once we draw, the memory
+    // allocated for the context for rendering will then contain the
+    // raw image data in the specified color space.
+    CGContextDrawImage(context, CGRectMake(0,0,width, height), imgRef);
+    
+    CGImageRef imgRef2 = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    free(bitmapData);
+    UIImage * image = [UIImage imageWithCGImage:imgRef2 scale:initImage.scale orientation:UIImageOrientationUp];
+    CGImageRelease(imgRef2);
+    return image;
 }
 
 #pragma mark - Action Events
